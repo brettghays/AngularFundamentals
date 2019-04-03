c420ad8a2d85c778fb264143a8f18bed9906b885
Brett Hays <bhays@simplifile.com>
2019-04-03 08:56:56 -0600

    Module 6

--- ng-fundamentals/src/app/events/events-list.component.ts
+++ ng-fundamentals/src/app/events/events-list.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from './../common/toastr.service';
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
@@ -6,7 +7,6 @@ declare let toastr;
 
 @Component
 ({
-  selector: 'events-list',
   template: `
     <div>
       <h1>Upcoming Angular Events</h1>
@@ -20,14 +20,14 @@ declare let toastr;
   `
 })
 export class EventsListComponent implements OnInit {
-  events:any[];
+  events:any;
 
-  constructor(private eventService: EventService, private toastr: ToastrService) {
+  constructor(private eventService: EventService, private toastr: ToastrService, private route: ActivatedRoute) {
 
   }
 
   ngOnInit() {
-    this.events = this.eventService.getEvents();
+    this.events = this.route.snapshot.data['events'];
   }
 
   handleThumbnailClick(eventName) {
